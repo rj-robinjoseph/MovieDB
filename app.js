9300f4019ed9ee0d0bcdebee166c4332a2552fc7
rj-robinjoseph <robinjoseph452@gmail.com>
2022-04-19 20:55:49 +0530

    Design Completed

---
+++ app.js
@@ -0,0 +1,14 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=45ea19d787f462846ede1e9f7540b5a1&page=1";
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=45ea19d787f462846ede1e9f7540b5a1&query="';
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+}
