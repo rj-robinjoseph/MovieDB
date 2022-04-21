d6d07f4110d0f7b6472533d94167e22d1a592be4
rj-robinjoseph <robinjoseph452@gmail.com>
2022-04-21 20:14:19 +0530

    Design Completed

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=45ea19d787f462846ede1e9f7540b5a1&query="';
 
+
+const search = document.getElementById(.searchIcon)
 async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
