ce90293006659dd451dc09da843522ff649c41bf
rj-robinjoseph <robinjoseph452@gmail.com>
2022-04-21 21:26:42 +0530

    API Completed

--- app.js
+++ app.js
@@ -6,11 +6,29 @@ const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=45ea19d787f462846ede1e9f7540b5a1&query="';
 
+const form = document.getElementById("form");
+const searchInput = document.getElementById("searchInput");
+const searchIcon = document.querySelector(".searchIcon");
 
-const search = document.getElementById(.searchIcon)
+getMovies(API_URL);
+
 async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
   console.log(data.results);
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = searchInput.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    searchInput.value = "";
+  } else {
+    window.location.reload();
+  }
+});
