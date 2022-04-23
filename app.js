6519d9b0e30908fe8d16a506d50eec9d7ec8f14a
rj-robinjoseph <robinjoseph452@gmail.com>
2022-04-23 17:42:17 +0530

    Working on sort using Genre

--- app.js
+++ app.js
@@ -1,14 +1,126 @@
+const API_KEY = "api_key=45ea19d787f462846ede1e9f7540b5a1";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=45ea19d787f462846ede1e9f7540b5a1&page=1";
+  BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY + "&page=1";
 
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
-const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?api_key=45ea19d787f462846ede1e9f7540b5a1&query="';
+const SEARCH_API = BASE_URL + "/search/movie?" + API_KEY;
 
+const genres = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
 const form = document.getElementById("form");
 const searchInput = document.getElementById("searchInput");
 const main = document.getElementById("main");
+const tagsEl = document.getElementById("tags");
+
+var selectedGenre = [];
+
+setGenre();
+function setGenre() {
+  tagsEl.innerHTML = "";
+  genres.forEach((genre) => {
+    const t = document.createElement("div");
+    t.classList.add("tag");
+    t.id = genre.id;
+    t.innerText = genre.name;
+    t.addEventListener("click", () => {
+      if (selectedGenre.length == 0) {
+        selectedGenre.push(genre.id);
+      } else {
+        if (selectedGenre.includes(genre.id)) {
+          selectedGenre.forEach((id, index) => {
+            if (id == genre.id) {
+              selectedGenre.splice(index, 1);
+            }
+          });
+        } else {
+          selectedGenre.push(genre.id);
+        }
+      }
+      console.log(selectedGenre);
+    });
+    tagsEl.append(t);
+  });
+}
 
 getMovies(API_URL);
 
@@ -64,7 +176,7 @@ form.addEventListener("submit", (e) => {
   const searchTerm = searchInput.value;
 
   if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + searchTerm);
+    getMovies(SEARCH_API + "&query=" + searchTerm);
 
     searchInput.value = "";
   } else {
